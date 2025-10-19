c059432425f0550ae034a1d33ec98ac655188603
timon <timongisler@outlook.com>
2025-10-19 22:46:47 +0200

    added discussion frequency in professional environment chart

--- src/pages/GeneralAwareness.tsx
+++ src/pages/GeneralAwareness.tsx
@@ -1,8 +1,16 @@
+import DiscussionFrequency from "./general-awareness/DiscussionFrequency";
+
 const GeneralAwareness = () => {
   return (
-    <h1 className="text-3xl font-semibold tracking-tight text-plum-500">
-      General Awareness of Sustainability
-    </h1>
+    <div className="space-y-8">
+      <h1 className="text-3xl font-semibold tracking-tight text-plum-500">
+        General Awareness of Sustainability
+      </h1>
+
+      <div className="grid grid-cols-1 gap-6">
+        <DiscussionFrequency />
+      </div>
+    </div>
   );
 };
 
