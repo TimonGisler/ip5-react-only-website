5512a0467ef98c1b8911a314559a5564145ad21f
timon <timongisler@outlook.com>
2025-10-19 23:07:07 +0200

    added graph for sustainability dimensions in software development projects

--- src/pages/role-of-digital-sustainability/DigitalSustainabilityRole.tsx
+++ src/pages/role-of-digital-sustainability/DigitalSustainabilityRole.tsx
@@ -1,8 +1,16 @@
+import SustainabilityDimensions from "./SustainabilityDimensions";
+
 const DigitalSustainabilityRole = () => {
   return (
-    <h1 className="text-3xl font-semibold tracking-tight text-plum-500">
-      The Role of Digital Sustainability in Your Organization
-    </h1>
+    <div className="space-y-8">
+      <h1 className="text-3xl font-semibold tracking-tight text-plum-500">
+        The Role of Digital Sustainability in Your Organization
+      </h1>
+
+      <div className="grid grid-cols-1 gap-6">
+        <SustainabilityDimensions />
+      </div>
+    </div>
   );
 };
 
